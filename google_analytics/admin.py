4fa17bbfa99064a71632fc8a4775e97cabdbadcf
Viktor Nagy <viktor.nagy@gmail.com>
2014-02-11 12:56:49 +0100

    Analytics is included under the Site admin

--- google_analytics/admin.py
+++ google_analytics/admin.py
@@ -1,4 +1,12 @@
-from google_analytics.models import Analytics
+from .models import Analytics
 from django.contrib import admin
+from django.contrib.sites.admin import SiteAdmin, Site
 
-admin.site.register(Analytics)
+class AnalyticsInlineAdmin(admin.StackedInline):
+    model = Analytics
+
+class AnalyticsSiteAdmin(SiteAdmin):
+    inlines = [AnalyticsInlineAdmin]
+
+admin.site.unregister(Site)
+admin.site.register(Site, AnalyticsSiteAdmin)
